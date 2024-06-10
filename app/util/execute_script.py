a019403b336232e5b11dbe79b6ce058c13d6eda2
Buddhika D Ranaweera <106642348+BuddhikaDilupul@users.noreply.github.com>
2024-06-10 07:49:46 -0300

    Update execute_script.py

--- app/util/execute_script.py
+++ app/util/execute_script.py
@@ -1,19 +1,17 @@
 import logging
 import subprocess
-
-
+ 
 def execute_script(script_path, service_name):
     try:
-        ansible_command = f"ansible-playbook {script_path} -e 'service={service_name}'"
-        # Execute the Ansible playbook using subprocess
-        subprocess.run(ansible_command, shell=True, check=True)
-
-
-        print("Ansible playbook executed successfully.")
-        logging.info("Ansible playbook executed successfully.")
+        bash_command = f"sudo -u ubuntu bash {script_path} {service_name}"
+        # Execute the bash script using subprocess
+        subprocess.run(bash_command, shell=True, check=True)
+ 
+        print("Bash script executed successfully.")
+        logging.info("Bash script executed successfully.")
         return True
     except subprocess.CalledProcessError as e:
         error_message = e.stderr if e.stderr else str(e)
-        print("Error executing Ansible playbook:", e)
-        logging.error("Error happned while executing the script.")
+        print("Error executing bash script:", error_message)
+        logging.error("Error happened while executing the script: %s", error_message)
         return False
